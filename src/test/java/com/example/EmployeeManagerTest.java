f652af1a32d346c4d38c667e0d3595d8a4e4e20f
Tahar Guemir <tahar.guemir@iths.se>
2021-04-06 21:56:27 +0200

    Upgift 2 done

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -33,10 +35,68 @@ void testEmployeeRepository(){
             new Employee("Jessica", 25000),
             new Employee("Tahar", 25000)));
         var actual = employeeRepository.findAll();
+        actual.get(0).setId("Gusta");
         var employees   = actual.stream()
-                .filter(employee -> employee.getId().equals("Gustav"))
+                .filter(employee -> employee.getId().equals("Gusta"))
                 .map((employeeId)->employeeId.getId())
                 .collect(Collectors.toList());
-        assertThat(employees).containsExactly(new Employee("Gustav", 25000).getId());
+        assertThat(employees).containsExactly(new Employee("Gusta", 25000).getId());
     }
-}
+    @Test
+    void testSalary() {
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        when(employeeRepository.findAll()).thenReturn(List.of(new Employee("Gustav", 25000),
+                new Employee("Jessica", 25000),
+                new Employee("Tahar", 25000)));
+        var actual = employeeRepository.findAll();
+        actual.get(0).setSalary(15000);
+        var employees = actual.stream()
+                .filter(employee -> employee.getId().equals("Gustav"))
+                .map((employeeId) -> employeeId.getSalary())
+                .collect(Collectors.toList());
+               assertThat(employees).containsExactly(new Employee("Gustav", 15000).getSalary());
+    }
+    @Test
+    void testISemployeepayed(){
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        when(employeeRepository.findAll()).thenReturn(List.of(new Employee( "Gustav", 25000),
+                new Employee("Jessica", 25000),
+                new Employee("Tahar", 25000)));
+        var actual = employeeRepository.findAll();
+        actual.get(0).setPaid(true);
+
+        assertTrue(actual.get(0).isPaid());
+    }
+    @Test
+    void testToString(){
+        Employee employee = new Employee("Martin", 25000.0);
+        String expected = "Employee [id=Martin, salary=25000.0]";
+        assertEquals(expected, employee.toString());
+    }
+    @Test
+    void testpayment() {
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        when(employeeRepository.findAll()).thenReturn(List.of(new Employee("Gustav", 25000),
+                new Employee("Jessica", 25000),
+                new Employee("Tahar", 25000)));
+        BankService bankService = new TestBankService();
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+        var expected = employeeManager.payEmployees();
+        assertEquals(expected, 3);
+    }
+        @Test
+        void testTrayCatch() {
+            EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+            when(employeeRepository.findAll()).thenReturn(List.of(new Employee("Gustav", 25000),
+                    new Employee("Jessica", 25000),
+                    new Employee("Martin", 25000),
+                    new Employee("Tahar", 25000)));
+
+            BankService bankService = new TestBankService();
+            EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+            var expected = employeeManager.payEmployees();
+            assertEquals(expected, 3);
+        }
+
+    }
+
--- src/test/java/com/example/TestBankService.java
+++ src/test/java/com/example/TestBankService.java
@@ -3,6 +3,10 @@ package com.example;
 public class TestBankService implements BankService{
     @Override
     public void pay(String id, double amount) {
+if (id.equals("Martin"))
+
+        throw new RuntimeException();
+
 
     }
 }
